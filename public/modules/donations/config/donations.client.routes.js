111dceba4acb2c8b5ffcc097c8ab6ea2aded2352
mukesh <pace.akash@gmail.com>
2015-05-02 02:01:41 +0300

    removed headers

--- public/modules/donations/config/donations.client.routes.js
+++ public/modules/donations/config/donations.client.routes.js
@@ -8,10 +8,10 @@ angular.module('donations').config(['$stateProvider',
     state('listDonations', {
         url: '/donations',
         templateUrl: 'modules/donations/views/list-donations.client.view.html'
-    }).
-    state('createDonation', {
-        url: '/donation/create',
-        templateUrl: 'modules/donations/views/create-donation.client.view.html'
     });
+    // state('createDonation', {
+    //     url: '/donation/create',
+    //     templateUrl: 'modules/donations/views/create-donation.client.view.html'
+    // });
 }
 ]);
